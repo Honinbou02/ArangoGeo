aa9e1a7aef5954456bcaeb885256a00883ff714d
Honinbou02 <maah.celo.v@gmail.com>
2025-07-11 00:12:21 -0300

    Update main.js

--- geo/main.js
+++ geo/main.js
@@ -1,9 +1,9 @@
 'use strict';
 const createRouter = require('@arangodb/foxx/router');
 const router = createRouter();
-module.context.use(router);
 
-// Load routes
+const fs = require('fs');
+
 require('./routes/near')(router);
 require('./routes/within')(router);
 require('./routes/contains')(router);
@@ -12,9 +12,10 @@ require('./routes/buffer')(router);
 require('./routes/join')(router);
 require('./routes/simplify')(router);
 
-const fs = require('fs');
-
 router.get('/ui', function (req, res) {
   res.set('Content-Type', 'text/html; charset=utf-8');
   res.body = fs.readFileSync(module.context.fileName('ui/index.html'), 'utf8');
 });
+
+// CORRIGIDO AQUI:
+module.context.use('/', router);
